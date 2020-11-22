18ef63b7834aa662808ca003f4dc2d615bac6320
Vadim <vadimtkachenkoik@gmail.com>
2020-11-22 20:43:47 +0300

    ХУЙ
    121212

---
+++ New Unity Project/Assets/Scripts/DropPlaceScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DropPlaceScript : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        CardScr card = eventData.pointerDrag.GetComponent<CardScr>();
+        if (card)
+            card.DefaultParent = transform;
+    }
+}
